de20021a93c59a7d6073437829702f6617090fb4
Tyler Nichols <tylernchls@gmail.com>
2017-01-15 14:21:58 -1000

    finished chirp chirp

--- js/chirp.js
+++ js/chirp.js
@@ -1,6 +1,13 @@
 function chirp(n){
   // FIX ME
-}
+  if(n === 1) {
+    return 'chirp';
+  }
+
+
+   return 'chirp ' + chirp(--n);
+
+};
 
 $(document).ready(function(){
   $("#result").html(chirp(3));
--- js/chirp.tests.js
+++ js/chirp.tests.js
@@ -1,10 +1,9 @@
 describe("The recursive sound of chirping", function() {
-  
+
   it("should chirp 3 times", function() {
-    
+
     var chirps = chirp(3);
-    
-    expect(chrips.trim()).toBe("chirp chirp chirp");
+    expect(chirps.trim()).toBe("chirp chirp chirp");
   });
 
 });
