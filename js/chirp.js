99df85c6e6dab0cfaa717d69da6961edf25f7324
Tyler Nichols <tylernchls@gmail.com>
2017-01-15 14:44:09 -1000

    added notes to chirp

--- js/chirp.js
+++ js/chirp.js
@@ -1,3 +1,10 @@
+/*
+Once base case is reached n==1 recursion starts n=1.
+n=3 'chirp' + chirp(2) => 'chirp chirp chirp'
+n=2 'chirp' + chirp(1) => 'chirp chirp'
+n=1 'chirp' => 'chirp'
+*/
+
 function chirp(n){
   // FIX ME
   if(n === 1) {
